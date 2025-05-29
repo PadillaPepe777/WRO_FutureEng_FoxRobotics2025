321d357824f65578e1d5b573999d6e56194674e5
PadillaPepe777 <125338833+PadillaPepe777@users.noreply.github.com>
2025-05-28 22:18:28 -0700

    Create Instructions.m

---
+++ ESP32Cam_ESP32_Serial/Instructions.m
@@ -0,0 +1,3 @@
+Here's a test code for ESPCAM and ESP32 Serial communication for the color tracking.
+We went for a simple solution where we separate the picture in 5 sector, sending only
+a two digit number to read both color and sector in the ESP32
